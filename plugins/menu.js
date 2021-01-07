6ee5ada9aa8ce86fd2709cfcf0ba9d0acd772494
MUH DENI SETIAWAN <77053529+jesxsky@users.noreply.github.com>
2021-01-07 17:28:48 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
   let preview = {}
   try {
-    if (!conn.menu) preview = await conn.generateLinkPreview('https://github.com/Arya274/Arya-Bot')
+    if (!conn.menu) preview = await conn.generateLinkPreview('https://github.com/jesxsky/cih')
   } catch (e) {
     try {
       if (!conn.menu) preview = await global.conn.generateLinkPreview('https://github.com/Nurutomo/wabot-aq')
@@ -32,8 +32,8 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
       .replace(/%week/g, week)
       .replace(/%date/g, date)
       .replace(/%time/g, time): `
-🤖 [NFQ BOT] 🤖
-Created Bye: @Drawl Nag
+🤖 [UTMs BOT] 🤖
+Created Bye: DENI
 
 Hi, ${name} 👋
 Exp: ${exp}
@@ -92,18 +92,16 @@ Cara Tambah XP:
 ╠➥ ${_p}mutechat group
 ║
 ╠═〘 IKLAN 〙 ═
-╠➥ Instagram: @arpunchs
-╠➥ Github: https://github.com/Arya274/Arya-Bot
+╠➥ Instagram: GOBLOK
 ║
 ╠═〘 Info Bot 〙 ═
-╠➥ Name : NFQ BOT
-╠➥ Coded using *Nano* on Android \\w Termux
-╠➥ Request? Wa.me/6288235435804
+╠➥ Name : UTMs BOT
+╠➥ Request? Wa.me/62882144323683
 ╠═════
 ║ Advanced:
 ║  > return m
 ║
-╠═〘 NfQ BOT 〙═
+╠═〘 UTMs BOT 〙═
 `.trim()
     conn.reply(m.chat, {...preview, text}, m)
   }
